4262248f05c3e5bade3f0ec2cfc3bdcd68c91085
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-05 15:22:30 -0700

    progress

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,24 @@
+
 import React from 'react';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import ArtistSearch from '../ArtistSearch.jsx';
+import Artist from '../Artist.jsx';
+import ArtistDisplay from '../../container/ArtistDisplay.jsx';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={ArtistDisplay}/>
+          <Route exact path='/' component={Artist}/>  
+        </Switch>
+      </Router>
+    </>
+  );
 }
   
---
+++ src/services/ApiFetch.jsx
@@ -0,0 +1,30 @@
+export const fetchArtist = artistSearch => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artistSearch}&fmt=json`)
+    .then(res => res.json())
+    .then(json => json.artists.map(artist => ({
+      id: artist.id,
+      name: artist.name
+    })));
+     
+};
+export const fetchAlbum = artistId => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`)
+    .then(res => res.json())
+    .then(json => json.releases.map(release => ({
+      releaseId: release.id,
+      releaseTitle: release.title,
+      releaseDate: release.date
+    })));
+};
+export const fetchSongs = releaseId => {
+  return fetch(`http://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`)
+    .then(res => res.json())
+    .then(json => json.recordings.map(song => ({
+      songId: song.id,
+      songTitle: song.title
+    })));
+};
+export const fetchLyrics = (artistName, songTitle) => {
+  return fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
+    .then(res => res.lyrics);
+};
